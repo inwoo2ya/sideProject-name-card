ee0d5b638dadc03d745c26e17137867e434c9685
inwoo2ya <dlsdn92@naver.com>
2024-09-16 17:00:10 +0900

    간단하게 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          boxShadow: "0px 2px 8px 0px rgba(0,0,0,0.3)",
+          display: "flex",
+          width: "900px",
+          height: "500px",
+          padding: "1.5rem 1rem",
+          borderRadius: "0.25rem",
+        }}
+      >
+        <div>
+          <h4>명함</h4>
+          <div>
+            이름: 이인우 Manager 전화번호: 010-2830-1103 이메일:
+            liw0627@lavarwave.co.kr 부서: Tech-본부 / 서비스 개발실(팀)
+            회사:(주)라바웨이브 www.lavarwave.co.kr 회사 위치: 서울특별시 강남구
+            헌릉로571길 32 (세곡동) 우편 번호: 06376 회사 번호: 02-1811-9634
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
